56b068df8a78a1fb02d31191612af9c9437750f1
leeMK09 <dlaudrb09@naver.com>
2025-01-09 13:20:24 +0900

    feat: 레이싱에 참가한 차량의 우승자를 조회할 수 있다

---
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/RacingCars.kt
@@ -0,0 +1,14 @@
+package org.gonza.kotlinplayground.domain
+
+class RacingCars(
+    private val carList: List<Car>
+) {
+    fun findWonCarList(): List<Car> {
+        val farthestPosition = carList.maxOf { it.getPosition() }
+        val farthestCarList = carList.filter { it.getPosition() == farthestPosition }
+
+        return farthestCarList.map {
+            Car(it.getPosition())
+        }
+    }
+}
---
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/RacingCarsTest.kt
@@ -0,0 +1,19 @@
+package org.gonza.kotlinplayground.domain
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class RacingCarsTest {
+    @Test
+    fun `우승한 차량을 조회할 수 있다`() {
+        val car1 = Car(1)
+        val car2 = Car(2)
+        val wonCar = Car(3)
+        val racingCars = RacingCars(listOf(car1, car2, wonCar))
+
+        val carList = racingCars.findWonCarList()
+
+        assertThat(carList).hasSize(1)
+        assertThat(carList[0].getPosition()).isEqualTo(wonCar.getPosition())
+    }
+}
