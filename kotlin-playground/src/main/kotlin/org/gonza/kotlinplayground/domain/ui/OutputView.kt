5cf7647b91b18018774dbd31a41dbd5e18dde038
leeMK09 <dlaudrb09@naver.com>
2025-01-15 13:58:35 +0900

    feat: 차량 이름과 시도 횟수에 대한 에러 메시지 출력 추가

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/ui/OutputView.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/ui/OutputView.kt
@@ -8,4 +8,12 @@ class OutputView {
     fun printTryCount() {
         println("시도할 회수는 몇회인가요?")
     }
+
+    fun printCarNameError() {
+        println("차량 이름을 잘 못 입력하였습니다.")
+    }
+
+    fun printTryCountError() {
+        println("시도 횟수를 잘 못 입력하였습니다.")
+    }
 }
