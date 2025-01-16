36fe3f1f97b64e237d810c15eeccda534e508513
leeMK09 <dlaudrb09@naver.com>
2025-01-16 13:56:53 +0900

    feat: 검증 실패인 경우 재입력 처리

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/PlayGround.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/PlayGround.kt
@@ -1,14 +1,46 @@
 package org.gonza.kotlinplayground
 
+import org.gonza.kotlinplayground.domain.car.exception.EmptyOrNullableCarNameException
 import org.gonza.kotlinplayground.ui.InputView
 import org.gonza.kotlinplayground.ui.OutputView
 
 fun main() {
     val output = OutputView()
     val input = InputView()
+    val validator = RacingCarGameValidator()
 
-    output.printInputCarNameList()
-    val carName = input.read()
-    output.printTryCount()
-    val tryCount = input.read()
+    val carNameByInput = getValidatedCarName(output, input, validator)
+    val tryCount = getValidatedTryCount(output, input, validator)
+}
+
+private fun getValidatedCarName(
+    output: OutputView,
+    input: InputView,
+    validator: RacingCarGameValidator
+): String {
+    while (true) {
+        try {
+            output.printInputCarNameList()
+            val carNameByInput = input.read()
+            return validator.getValidatedCarName(carNameByInput)
+        } catch (e: EmptyOrNullableCarNameException) {
+            output.printCarNameError()
+        }
+    }
+}
+
+private fun getValidatedTryCount(
+    output: OutputView,
+    input: InputView,
+    validator: RacingCarGameValidator
+): Int {
+    while (true) {
+        try {
+            output.printTryCount()
+            val tryCountByInput = input.read()
+            return validator.getValidatedTryCountString(tryCountByInput)
+        } catch (e: EmptyOrNullableCarNameException) {
+            output.printTryCount()
+        }
+    }
 }
