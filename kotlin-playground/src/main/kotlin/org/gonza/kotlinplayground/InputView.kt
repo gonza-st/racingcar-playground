993c1dbc28ddc5e5f88c9c236e8ef79df1eec4e8
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2025-01-16 12:43:27 +0900

    feat: 입출력 추가
    입출력 추가

---
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/InputView.kt
@@ -0,0 +1,17 @@
+package org.gonza.kotlinplayground
+
+import java.util.Scanner
+
+class InputView {
+    fun carNames(): String {
+        val scanner = Scanner(System.`in`)
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return scanner.next()
+    }
+
+    fun racingCount(): Int {
+        val scanner = Scanner(System.`in`)
+        println("시도할 회수는 몇 회인가요?")
+        return scanner.nextInt()
+    }
+}
---
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/OutputView.kt
@@ -0,0 +1,16 @@
+package org.gonza.kotlinplayground
+
+class OutputView {
+    fun roundPosition(raceResult: List<Cars>) {
+        println("실행 결과")
+        for (cars in raceResult) {
+            println(cars.positions())
+            println()
+        }
+    }
+
+    fun winner(raceResult: List<Cars>) {
+        val winners = raceResult.last().winners()
+        println("${winners}가 최종 우승했습니다.")
+    }
+}
