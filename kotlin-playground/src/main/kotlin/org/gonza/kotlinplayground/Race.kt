90e46e1f21f2f3f7c0ffb4c08fc8cb0e5384fa83
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2025-01-16 12:41:21 +0900

    feat: 자동차 경주 추가 및 검증
    자동차 경주 추가 및 검증

---
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/Race.kt
@@ -0,0 +1,63 @@
+package org.gonza.kotlinplayground
+
+class Race(
+    private val carList: List<Car>,
+    private var raceTimes: RaceTimes,
+    private var raceResult: MutableList<Cars> = mutableListOf(),
+) {
+    constructor(
+        carNames: String,
+        raceTimes: Int,
+        numberGenerator: NumberGenerator,
+    ) : this(
+        carList = NameSplitter.split(carNames).map { Car(it, numberGenerator) },
+        raceTimes = RaceTimes(raceTimes),
+    )
+
+    fun run() {
+        while (this.raceTimes.isNotZero()) {
+            this.proceed()
+            val currentCars =
+                Cars(
+                    this.carList.map { car ->
+                        Car(
+                            name = car.name,
+                            position = Position(car.position()),
+                            numberGenerator = car.numberGenerator,
+                        )
+                    },
+                )
+            this.raceResult.add(currentCars)
+            this.raceTimes = this.raceTimes.decrease()
+        }
+    }
+
+    fun timesEqualTo(expectedValue: Int): Boolean = this.raceTimes.equal(expectedValue)
+
+    fun raceResult(): List<Cars> = this.raceResult
+
+    internal fun proceed(): List<Car> {
+        this.carList.forEach { it.move() }
+        return this.carList
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Race) return false
+
+        if (raceTimes != other.raceTimes) return false
+        if (carList != other.carList) return false
+        if (raceResult != other.raceResult) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = raceTimes.hashCode()
+        result = 31 * result + carList.hashCode()
+        result = 31 * result + raceResult.hashCode()
+        return result
+    }
+
+    override fun toString(): String = "Race(carList=$carList, raceTimes=$raceTimes, raceResult=$raceResult)"
+}
---
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/RaceTest.kt
@@ -0,0 +1,86 @@
+package org.gonza.kotlinplayground
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class RaceTest {
+    @Test
+    fun `한번 진행시키면 현재 상태 값을 가진 차들을 반환한다`() {
+        val numberGenerator = MovedNumberGenerator()
+        val race =
+            Race(
+                carNames = "아반떼, 소나타, 그랜저",
+                raceTimes = 1,
+                numberGenerator = numberGenerator,
+            )
+
+        val carList = race.proceed()
+        for (car in carList) {
+            println(car)
+        }
+
+        val expectedCarList =
+            listOf(
+                Car("아반떼", Position(1), numberGenerator),
+                Car("소나타", Position(1), numberGenerator),
+                Car("그랜저", Position(1), numberGenerator),
+            )
+
+        Assertions.assertThat(carList).isEqualTo(expectedCarList)
+    }
+
+    @Test
+    fun `경주가 전체 진행되면 raceTimes는 0이 된다`() {
+        val numberGenerator = MovedNumberGenerator()
+        val race =
+            Race(
+                carNames = "아반떼, 소나타, 그랜저",
+                raceTimes = 5,
+                numberGenerator = numberGenerator,
+            )
+        race.run()
+
+        val expectedValue = 0
+
+        Assertions.assertThat(race.timesEqualTo(expectedValue)).isEqualTo(true)
+    }
+
+    @Test
+    fun `경주가 진행되면 raceResult에 결과값들이 쌓인다`() {
+        val numberGenerator = MovedNumberGenerator()
+        val race =
+            Race(
+                carNames = "아반떼, 소나타, 그랜저",
+                raceTimes = 3,
+                numberGenerator = numberGenerator,
+            )
+        race.run()
+
+        val expectedCars =
+            listOf(
+                Cars(
+                    listOf(
+                        Car("아반떼", Position(1), numberGenerator),
+                        Car("소나타", Position(1), numberGenerator),
+                        Car("그랜저", Position(1), numberGenerator),
+                    ),
+                ),
+                Cars(
+                    listOf(
+                        Car("아반떼", Position(2), numberGenerator),
+                        Car("소나타", Position(2), numberGenerator),
+                        Car("그랜저", Position(2), numberGenerator),
+                    ),
+                ),
+                Cars(
+                    listOf(
+                        Car("아반떼", Position(3), numberGenerator),
+                        Car("소나타", Position(3), numberGenerator),
+                        Car("그랜저", Position(3), numberGenerator),
+                    ),
+                ),
+            )
+
+        Assertions.assertThat(race.raceResult()).isEqualTo(expectedCars)
+    }
+}
