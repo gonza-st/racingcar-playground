3019d4597af81c7863a306fbfdca55f44d5d0ad4
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2025-01-16 12:43:41 +0900

    feat: 게임 실행 추가
    게임 실행 추가

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/KotlinPlaygroundApplication.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/KotlinPlaygroundApplication.kt
@@ -8,4 +8,21 @@ class KotlinPlaygroundApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinPlaygroundApplication>(*args)
+    val input = InputView()
+    val output = OutputView()
+
+    val carNames: String = input.carNames()
+    val racingCount: Int = input.racingCount()
+
+    val race =
+        Race(
+            carNames = carNames,
+            raceTimes = racingCount,
+            numberGenerator = RandomNumberGenerator(),
+        )
+
+    race.run()
+
+    output.roundPosition(race.raceResult())
+    output.winner(race.raceResult())
 }
