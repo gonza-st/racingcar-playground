9f723e841395bcbd72ec9101388458c50e4b16ae
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2025-01-16 12:37:31 +0900

    feat: 자동차 이름 split하는 객체 추가 및 검증
    자동차 이름 split하는 객체 추가 및 검증

---
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/NameSplitter.kt
@@ -0,0 +1,30 @@
+package org.gonza.kotlinplayground
+
+class NameSplitter {
+    companion object {
+        private const val SPLIT_DELIMITER = ","
+        private const val BLANK_STRING = ""
+
+        fun split(value: String): List<String> {
+            if (value.isEmpty()) {
+                throw IllegalArgumentException("이름은 빈 문자열일 수 없습니다")
+            }
+            val nameList = value.split(SPLIT_DELIMITER).map { it.trim() }
+            validate(nameList)
+
+            return nameList
+        }
+
+        private fun validate(nameList: List<String>) {
+            val nameSet = nameList.toSet()
+
+            if (nameSet.contains(BLANK_STRING)) {
+                throw IllegalArgumentException("이름은 빈 문자열일 수 없습니다")
+            }
+
+            if (nameSet.size != nameList.size) {
+                throw IllegalArgumentException("이름은 중복될 수 없습니다")
+            }
+        }
+    }
+}
