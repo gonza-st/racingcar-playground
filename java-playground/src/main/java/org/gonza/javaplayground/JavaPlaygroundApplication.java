d78f929eb627fc36336e6e56c675c107b89e5048
HeoJun <parkteammini@gmail.com>
2025-01-16 13:27:37 +0900

    feat: 자동차 경주 게임 도메인 및 테스트 추가
    자동차 경주 게임 도메인 객체(Game)를 구현하고 관련 테스트(GameTest)를 추가하였습니다.
    
    Game 객체는 입력된 자동차와 라운드 정보로 게임을 설정(setup)하고 실행(play)하며, 최종 승자를 계산하는 기능을 포함합니다.
    GameTest를 통해 게임 초기화 및 승자 검증 로직이 올바르게 동작하는지 테스트를 작성하였습니다.

--- java-playground/src/main/java/org/gonza/javaplayground/JavaPlaygroundApplication.java
+++ java-playground/src/main/java/org/gonza/javaplayground/JavaPlaygroundApplication.java
@@ -1,13 +1,13 @@
 package org.gonza.javaplayground;
 
-import org.springframework.boot.SpringApplication;
+import org.gonza.javaplayground.domain.Game;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaPlaygroundApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        Game game = new Game().setup();
+        game.play();
+    }
 }
---
+++ java-playground/src/main/java/org/gonza/javaplayground/domain/Game.java
@@ -0,0 +1,82 @@
+package org.gonza.javaplayground.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import org.gonza.javaplayground.util.Random;
+
+public class Game {
+    private Cars cars;
+    private int round;
+
+    public Game() {
+    }
+
+    public Game setup() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        Scanner sc = new Scanner(System.in);
+        String carNames = sc.nextLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        int round = sc.nextInt();
+        sc.close();
+
+        Cars cars = new Cars(carNames);
+        System.out.println(Arrays.toString(cars.getCars().toArray()));
+
+        this.cars = cars;
+        this.round = round;
+
+        return this;
+    }
+
+    public void play() {
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < this.getRound(); i++) {
+            for (Car car : this.getCars().getCars()) {
+                randomMoveCar(car);
+                System.out.println(car.getName() + ": " + "-".repeat(car.getLocation()));
+            }
+            System.out.println();
+        }
+        this.endGame();
+    }
+
+    public void endGame() {
+        this.getWinner().forEach(car -> {
+            System.out.print(car.getName());
+        });
+        System.out.println("가 승리하였습니다!.");
+    }
+
+    public ArrayList<Car> getWinner() {
+        ArrayList<Car> winners = new ArrayList<Car>();
+
+        cars.getCars().sort((car1, car2) -> car2.getLocation().compareTo(car1.getLocation()));
+
+        int topLocation = cars.getCars().getFirst().getLocation();
+
+        for (Car car : cars.getCars()) {
+            if (car.getLocation() == topLocation) {
+                winners.add(car);
+            }
+        }
+
+        return winners;
+    }
+
+    private static void randomMoveCar(Car car) {
+        Integer random = Random.between(1, 10);
+        if (Car.isPossibleMovingForward(random)) {
+            car.moveForward();
+        }
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public Cars getCars() {
+        return cars;
+    }
+}
