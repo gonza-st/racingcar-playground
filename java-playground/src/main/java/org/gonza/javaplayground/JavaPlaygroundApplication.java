db77b6a203958a24a710b4beed3427af602e61a5
hongxeob <sjun2918@naver.com>
2025-01-17 14:04:53 +0900

    refactor: 15. 기능 구현 완성

--- java-playground/src/main/java/org/gonza/javaplayground/JavaPlaygroundApplication.java
+++ java-playground/src/main/java/org/gonza/javaplayground/JavaPlaygroundApplication.java
@@ -1,13 +1,44 @@
 package org.gonza.javaplayground;
 
+import org.gonza.javaplayground.core.PlayerInput;
+import org.gonza.javaplayground.service.RacingGame;
+import org.gonza.javaplayground.util.Converter;
+import org.gonza.javaplayground.view.ConsolePrinter;
+import org.gonza.javaplayground.view.ConsoleReader;
+import org.gonza.javaplayground.view.Reader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+import static org.gonza.javaplayground.view.ConsolePrinter.printCars;
+import static org.gonza.javaplayground.view.ConsolePrinter.printWinners;
 
 @SpringBootApplication
 public class JavaPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaPlaygroundApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPlaygroundApplication.class, args);
+
+        Scanner scanner = new Scanner(System.in);
+        Reader consoleReader = new ConsoleReader(scanner);
+        ConsolePrinter consolePrinter = new ConsolePrinter();
+        consolePrinter.print("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carNames = consoleReader.read();
+        consolePrinter.print(carNames);
+        consolePrinter.print("시도할 회수는 몇회인가요?");
+        String retry = consoleReader.read();
+        PlayerInput playerInput = new PlayerInput(carNames, Integer.parseInt(retry));
+
+        RacingGame racingGame = new RacingGame(Converter.separatedByCommas(carNames));
+        racingGame.play();
+
+        for (int i = 0; i < playerInput.getRetry(); i++) {
+            racingGame.play();
+
+            printCars(racingGame);
+        }
+        printWinners(racingGame.winnerNames());
+    }
 
 }
--- java-playground/src/main/java/org/gonza/javaplayground/view/ConsolePrinter.java
+++ java-playground/src/main/java/org/gonza/javaplayground/view/ConsolePrinter.java
@@ -1,8 +1,49 @@
 package org.gonza.javaplayground.view;
 
+import org.gonza.javaplayground.core.Nickname;
+import org.gonza.javaplayground.core.Position;
+import org.gonza.javaplayground.service.RacingGame;
+
+import java.util.List;
+
 public class ConsolePrinter implements Printer {
+    private static final char CAR_DISTANCE_CHARACTER = '-';
+    private static final String PRINT_DELIMITER = " : ";
+
     @Override
     public void print(String message) {
         System.out.println(message);
+    }
+
+    public static void printCars(RacingGame cars) {
+        printCars(cars.getCarNames(), cars.getCarPositions());
+        printNewLine();
+    }
+
+    public static void printWinners(List<String> names) {
+        final String winnerNames = Nickname.parseWinnerNames(names);
+
+        System.out.println(winnerNames + "가 최종 우승했습니다.");
+    }
+
+    public static void printNewLine() {
+        System.out.println();
+    }
+
+    private static void printCars(List<String> names, List<Position> positions) {
+        final int size = names.size();
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(names.get(i));
+            System.out.print(PRINT_DELIMITER);
+            printPositions(positions.get(i));
+            printNewLine();
+        }
+    }
+
+    private static void printPositions(Position position) {
+        for (int i = 0; i < position.getDistance(); i++) {
+            System.out.print(CAR_DISTANCE_CHARACTER);
+        }
     }
 }
