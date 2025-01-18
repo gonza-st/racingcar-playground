0100718f9a13c82a014712ecd99dd803a40500c2
hongxeob <sjun2918@naver.com>
2025-01-18 15:56:38 +0900

    refactor: 18. PlayerInput 유효성 검사 추가 및 테스트코드 추가

--- java-playground/src/main/java/org/gonza/javaplayground/core/PlayerInput.java
+++ java-playground/src/main/java/org/gonza/javaplayground/core/PlayerInput.java
@@ -1,6 +1,5 @@
 package org.gonza.javaplayground.core;
 
-import org.gonza.javaplayground.util.Converter;
 
 import java.util.List;
 
@@ -8,19 +7,29 @@ public class PlayerInput {
     public static final int MINIMUM_RETRY_LIMIT = 0;
     public static final int MINIMUM_CARS = 2;
 
-    private String names;
+    private List<String> names;
     private int retry;
 
-    public PlayerInput(String names, int retry) {
+    public PlayerInput(List<String> names, int retry) {
         validateNames(names);
         validateRetry(retry);
         this.names = names;
         this.retry = retry;
     }
 
-    private void validateNames(String names) {
-        List<String> carNames = Converter.separatedByCommas(names);
-        if (carNames.size() < MINIMUM_CARS) {
+    public int getRetry() {
+        return retry;
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    private void validateNames(List<String> names) {
+        if (names == null) {
+            throw new IllegalArgumentException("자동차 이름 목록이 null일 수 없습니다.");
+        }
+        if (names.size() < MINIMUM_CARS) {
             throw new IllegalArgumentException("경주할 자동차는 최소 2대 이상이어야 합니다.");
         }
     }
@@ -29,13 +38,5 @@ public class PlayerInput {
         if (retry < MINIMUM_RETRY_LIMIT) {
             throw new IllegalArgumentException("재시도 횟수는 0미만일 수 없습니다.");
         }
-    }
-
-    public String getNames() {
-        return names;
-    }
-
-    public int getRetry() {
-        return retry;
     }
 }
