5485472699712df27971ccedef14bf7cab7ccfea
han-0624 <han@wemeetmobility.com>
2025-01-19 22:12:26 +0900

    feat: 요구사항 검증

--- java-playground/src/test/java/org/gonza/javaplayground/RequirementsTest.java
+++ java-playground/src/test/java/org/gonza/javaplayground/RequirementsTest.java
@@ -1,14 +1,65 @@
 package org.gonza.javaplayground;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.PrintStream;
+import java.util.Random;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class RequirementsTest {
+    @Mock
+    private Scanner scanner;
+
+    @Mock
+    private PrintStream printStream;
+
+    @Mock
+    private Random random;
+
+    private Game sut;
+
+    @BeforeEach
+    public void setup() {
+        sut = new Game(printStream, scanner, random);
+    }
+
     @Test
     public void 차량이름은_5자를_초과할_수_없다() {
+        Integer count = 1;
+        when(scanner.nextInt()).thenReturn(count);
+
+        String names = "too-long-name";
+        when(scanner.nextLine()).thenReturn(names);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            sut.race();
+        });
     }
 
     @Test
     public void 전진하는_자동차를_출력할_때_자동차_이름을_같이_출력한다() {
+        Integer count = 1;
+        when(scanner.nextInt()).thenReturn(count);
+
+        String names = "car1";
+        when(scanner.nextLine()).thenReturn(names);
+
+        sut.race();
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(printStream, times(2)).println(captor.capture());
+
+        assertTrue(captor.getAllValues().get(0).contains(names));
     }
 
     @Test
@@ -21,10 +72,36 @@ public class RequirementsTest {
 
     @Test
     public void 전진하는_조건은_random_값이_4이상일_경우이다() {
+        when(random.nextInt(10))
+                .thenReturn(1)
+                .thenReturn(4);
+
+        Integer count = 2;
+        when(scanner.nextInt()).thenReturn(count);
+
+        String names = "car1";
+        when(scanner.nextLine()).thenReturn(names);
+
+        sut.race();
+
+        verify(printStream).println("car1:" + 0);
+        verify(printStream).println("car1:" + 1);
     }
 
     @Test
     public void 자동차_경주_완료_후_우승자를_알려준다() {
-    }
+        when(random.nextInt(10))
+                .thenReturn(1)
+                .thenReturn(4);
 
+        Integer count = 2;
+        when(scanner.nextInt()).thenReturn(count);
+
+        String names = "car1";
+        when(scanner.nextLine()).thenReturn(names);
+
+        sut.race();
+
+        verify(printStream).println("car1");
+    }
 }
