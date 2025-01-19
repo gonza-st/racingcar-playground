82baffeef0a0b9c695641d8cd4e3ac2ffc314a96
currenjin <hyun0524e@naver.com>
2025-01-19 23:11:07 +0900

    Rename: Cars -> Racing

--- java-playground/src/main/java/org/gonza/javaplayground/car/Racing.java
+++ java-playground/src/main/java/org/gonza/javaplayground/car/Racing.java
@@ -3,17 +3,17 @@ package org.gonza.javaplayground.car;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cars {
+public class Racing {
     public static final int MOVE_CONDITION = 4;
 
-    private List<Car> carList = new ArrayList<>();
+    private List<Car> players = new ArrayList<>();
 
-    public Cars(List<String> names) {
-        this.carList = convertToCar(names);
+    public Racing(List<String> names) {
+        this.players = convertToCar(names);
     }
 
-    public int countCars() {
-        return this.carList.size();
+    public int countPlayers() {
+        return this.players.size();
     }
 
     private List<Car> convertToCar(List<String> names) {
@@ -30,9 +30,9 @@ public class Cars {
 
     public RaceResult race(List<Integer> numbers) {
         for (int i = 0; i < numbers.size(); i++) {
-            moveByNumberCondition(numbers.get(i), carList.get(i));
+            moveByNumberCondition(numbers.get(i), players.get(i));
         }
 
-        return new RaceResult(carList);
+        return new RaceResult(players);
     }
 }
--- java-playground/src/test/java/org/gonza/javaplayground/car/RacingTest.java
+++ java-playground/src/test/java/org/gonza/javaplayground/car/RacingTest.java
@@ -7,62 +7,62 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class CarsTest {
+class RacingTest {
     @Test
-    void Cars는_Car를_생성할_수_있다() {
+    void Racing은_Car를_생성할_수_있다() {
         List<String> names = List.of("test1", "test2", "test3");
 
-        Cars cars = new Cars(names);
+        Racing racing = new Racing(names);
 
-        assertEquals(names.size(), cars.countCars());
+        assertEquals(names.size(), racing.countPlayers());
     }
 
     @Test
     void Car의_이름이_6자를_초과하면_예외가_발생한다() {
         List<String> names = List.of("123456");
 
-        assertThrows(IllegalArgumentException.class, () -> new Cars(names));
+        assertThrows(IllegalArgumentException.class, () -> new Racing(names));
     }
 
     @Test
     void 숫자가_4_이상인_Car만_이동한다() {
-        Cars cars = new Cars(List.of("test1", "test2"));
+        Racing racing = new Racing(List.of("test1", "test2"));
 
-        RaceResult result = cars.race(List.of(3, 4));
+        RaceResult result = racing.race(List.of(3, 4));
 
         assertEquals(1, result.getHighestCar().getFirst().getPosition());
         assertEquals("test2", result.getHighestCar().getFirst().getName());
     }
 
     @Test
-    void Cars의_Car는_숫자_4_이상이면_이동한다() {
-        Cars cars = new Cars(List.of("test1"));
+    void Racing의_Car는_숫자_4_이상이면_이동한다() {
+        Racing racing = new Racing(List.of("test1"));
 
-        RaceResult result = cars.race(List.of(4));
+        RaceResult result = racing.race(List.of(4));
 
         assertEquals(1, result.getHighestCar().getFirst().getPosition());
     }
 
     @Test
-    void Cars의_Car는_숫자_4_미만이면_이동하지_않는다() {
-        Cars cars = new Cars(List.of("test1"));
+    void Racing의_Car는_숫자_4_미만이면_이동하지_않는다() {
+        Racing racing = new Racing(List.of("test1"));
 
-        RaceResult result = cars.race(List.of(3));
+        RaceResult result = racing.race(List.of(3));
 
         assertEquals(0, result.getHighestCar().getFirst().getPosition());
     }
 
     @Test
     void 결과를_출력할_수_있다() {
-        Cars cars = new Cars(List.of("test1", "test2"));
+        Racing racing = new Racing(List.of("test1", "test2"));
 
-        RaceResult firstResult = cars.race(List.of(4, 4));
+        RaceResult firstResult = racing.race(List.of(4, 4));
         assertEquals("test1 : -\ntest2 : -", firstResult.report());
 
-        RaceResult secondResult = cars.race(List.of(4, 4));
+        RaceResult secondResult = racing.race(List.of(4, 4));
         assertEquals("test1 : --\ntest2 : --", secondResult.report());
 
-        RaceResult thirdResult = cars.race(List.of(3, 4));
+        RaceResult thirdResult = racing.race(List.of(3, 4));
         assertEquals("test1 : --\ntest2 : ---", thirdResult.report());
     }
 }
