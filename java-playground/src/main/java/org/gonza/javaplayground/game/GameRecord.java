bfd7347305b868995f7bd6f5ed393963671c0ab7
han-0624 <han@wemeetmobility.com>
2025-01-19 23:06:25 +0900

    feat: 기록 클래스의 요구사항 정리

---
+++ java-playground/src/main/java/org/gonza/javaplayground/game/GameRecord.java
@@ -0,0 +1,4 @@
+package org.gonza.javaplayground.game;
+
+public class GameRecord {
+}
---
+++ java-playground/src/test/java/org/gonza/javaplayground/game/GameRecordTest.java
@@ -0,0 +1,25 @@
+package org.gonza.javaplayground.game;
+
+import org.junit.jupiter.api.Test;
+
+public class GameRecordTest {
+
+    @Test
+    public void 차량별_누적_이동횟수를_조회할_수_있다() {}
+
+    @Test
+    public void 누적횟수가_없으면_0을_반환한다() {}
+
+    @Test
+    public void 누적횟수를_증가시킬수_있다() {}
+
+    @Test
+    public void 누적횟수가_가장_높은_차량을_조회할_수_있다() {
+    }
+
+    @Test
+    public void 누적횟수가_가장_높은_차량은_복수일_수_있다() {}
+
+    @Test
+    public void 누적횟수가_가장_높은_차량은_내림차순_정렬된다() {}
+}
