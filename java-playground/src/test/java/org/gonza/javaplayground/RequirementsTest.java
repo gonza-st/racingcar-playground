c980b4c6b217c43abfc67a22869b63ac40627b90
han-0624 <han@wemeetmobility.com>
2025-01-19 20:54:23 +0900

    chore: 기능 요구사항 정리

---
+++ java-playground/src/test/java/org/gonza/javaplayground/RequirementsTest.java
@@ -0,0 +1,30 @@
+package org.gonza.javaplayground;
+
+import org.junit.jupiter.api.Test;
+
+public class RequirementsTest {
+    @Test
+    public void 차량이름은_5자를_초과할_수_없다() {
+    }
+
+    @Test
+    public void 전진하는_자동차를_출력할_때_자동차_이름을_같이_출력한다() {
+    }
+
+    @Test
+    public void 자동차_이름은_comma로_구분한다() {
+    }
+
+    @Test
+    public void 전진하는_조건은_0_9_사이의_랜덤값_이어야한다() {
+    }
+
+    @Test
+    public void 전진하는_조건은_random_값이_4이상일_경우이다() {
+    }
+
+    @Test
+    public void 자동차_경주_완료_후_우승자를_알려준다() {
+    }
+
+}
