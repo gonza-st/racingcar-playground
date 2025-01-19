d47c390d6fb9a6f716892ca0410e2476f5d983fa
hongxeob <sjun2918@naver.com>
2025-01-19 16:39:40 +0900

    refactor: 25. 메인 클래스 수정

--- java-playground/src/main/java/org/gonza/javaplayground/JavaPlaygroundApplication.java
+++ java-playground/src/main/java/org/gonza/javaplayground/JavaPlaygroundApplication.java
@@ -1,14 +1,15 @@
 package org.gonza.javaplayground;
 
 import org.gonza.javaplayground.core.PlayerInput;
-import org.gonza.javaplayground.service.GameInputManager;
+import org.gonza.javaplayground.service.GameInputView;
 import org.gonza.javaplayground.service.RacingGame;
+import org.gonza.javaplayground.service.RacingGameRunner;
+import org.gonza.javaplayground.view.ConsolePrinter;
 import org.gonza.javaplayground.view.ConsoleReader;
+import org.gonza.javaplayground.view.Printer;
+import org.gonza.javaplayground.view.Reader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static org.gonza.javaplayground.view.ConsolePrinter.printCars;
-import static org.gonza.javaplayground.view.ConsolePrinter.printWinners;
 
 @SpringBootApplication
 public class JavaPlaygroundApplication {
@@ -16,19 +17,13 @@ public class JavaPlaygroundApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaPlaygroundApplication.class, args);
 
-        ConsoleReader reader = new ConsoleReader();
-        GameInputManager inputManager = new GameInputManager(reader);
-        PlayerInput playerInput = inputManager.createPlayerInput();
+        Reader reader = new ConsoleReader();
+        final Printer printer = new ConsolePrinter();
+        GameInputView inputView = new GameInputView(reader, printer);
+        PlayerInput playerInput = inputView.createPlayerInput();
 
         RacingGame racingGame = new RacingGame(playerInput.getNames());
-        racingGame.play();
-
-        for (int i = 0; i < playerInput.getRetry(); i++) {
-            racingGame.play();
-
-            printCars(racingGame);
-        }
-        printWinners(racingGame.winnerNames());
+        final RacingGameRunner racingGameRunner = new RacingGameRunner(racingGame, playerInput.getRetry(), printer);
+        racingGameRunner.run();
     }
-
 }
