02eec34ed2b82c97c6f791360fd5708216cad447
hongxeob <sjun2918@naver.com>
2025-01-19 16:37:20 +0900

    refactor: 22. 미사용 상수 제거

--- java-playground/src/main/java/org/gonza/javaplayground/util/Converter.java
+++ java-playground/src/main/java/org/gonza/javaplayground/util/Converter.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Converter {
 
     public static final String COMMAS = ",";
-    public static final int NUMBER_WHEN_NOT_SEPARATED = 1;
 
     public Converter() {
         throw new IllegalStateException("유틸리티 클래스는 인스턴스화 할 수 없습니다.");
