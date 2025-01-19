36900a5aebd6af285c58f9a3dbedcc8e53d3a19b
hongxeob <sjun2918@naver.com>
2025-01-19 16:42:46 +0900

    refactor: 26. 컨버터 인스턴스화 못하도록 수정

--- java-playground/src/main/java/org/gonza/javaplayground/util/NumberGenerator.java
+++ java-playground/src/main/java/org/gonza/javaplayground/util/NumberGenerator.java
@@ -3,6 +3,10 @@ package org.gonza.javaplayground.util;
 import java.util.Random;
 
 public class NumberGenerator {
+    public NumberGenerator() {
+        throw new IllegalStateException("유틸리티 클래스는 인스턴스화 할 수 없습니다.");
+    }
+
     public static int generateRandomNumber() {
         return new Random().nextInt(10);
     }
