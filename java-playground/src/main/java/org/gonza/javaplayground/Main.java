273e32cf25d3a91b99e16a74eac3a1fe31b89293
han-0624 <han@wemeetmobility.com>
2025-01-22 02:46:18 +0900

    feat: 실행 파일 작성

--- java-playground/src/main/java/org/gonza/javaplayground/Main.java
+++ java-playground/src/main/java/org/gonza/javaplayground/Main.java
@@ -1,8 +1,63 @@
 package org.gonza.javaplayground;
 
+import org.gonza.javaplayground.game.Game;
+import org.gonza.javaplayground.game.GamePrinter;
+import org.gonza.javaplayground.game.car.CarNameReader;
+import org.gonza.javaplayground.game.car.CarService;
+import org.gonza.javaplayground.game.car.CarSpec;
+import org.gonza.javaplayground.game.count.RacingCountReader;
+import org.gonza.javaplayground.game.count.RacingCountService;
+import org.gonza.javaplayground.game.record.GameRecord;
+import org.gonza.javaplayground.io.CarNameReaderImpl;
+import org.gonza.javaplayground.io.GamePrinterImpl;
+import org.gonza.javaplayground.io.RacingCountReaderImpl;
+import org.gonza.javaplayground.io.handler.IOHandler;
+
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		IOHandler ioHandler = createIOHandler();
+		CarService carService = createCarService(ioHandler);
+		RacingCountService racingCountService = createRacingCountService(ioHandler);
+		GamePrinter gamePrinter = createGamePrinter(ioHandler);
+		GameRecord gameRecord = new GameRecord();
+
+		Game game = new Game(carService, racingCountService, gamePrinter);
+
+		game.race();
+
+	}
+
+	private static GamePrinter createGamePrinter(IOHandler ioHandler) {
+		GamePrinter gamePrinter = new GamePrinterImpl(ioHandler);
+		return gamePrinter;
+	}
+
+	private static RacingCountService createRacingCountService(IOHandler ioHandler) {
+		RacingCountReader racingCountReader = new RacingCountReaderImpl(ioHandler);
+		RacingCountService service = new RacingCountService(racingCountReader);
+		return service;
+	}
+
+	private static CarService createCarService(IOHandler ioHandler) {
+		final Integer MAX_NAME_LENGTH = 5;
+		final Integer MAX_DISTANCE = 5;
+
+
+		CarNameReader carNameReader = new CarNameReaderImpl(ioHandler);
+		CarSpec carSpec = new CarSpec(MAX_NAME_LENGTH, MAX_DISTANCE);
+		CarService carService = new CarService(carNameReader, carSpec);
+
+		return carService;
+	}
+
+	private static IOHandler createIOHandler() {
+		Scanner scanner = new Scanner(System.in);
+		PrintStream out = System.out;
+
+		IOHandler ioHandler = new IOHandler(scanner, out);
+		return ioHandler;
 	}
 }
