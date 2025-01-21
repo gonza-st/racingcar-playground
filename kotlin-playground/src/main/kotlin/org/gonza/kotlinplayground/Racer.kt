044c22c06d28bcfb8b611cdd6dc3fb33546d204f
taiyoung <qq3222@naver.com>
2025-01-21 22:37:37 +0900

    feature: racer race 기능 작성

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/Racer.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/Racer.kt
@@ -2,4 +2,11 @@ package org.gonza.kotlinplayground
 
 class Racer(
     val car: Car,
-)
+    private val accelerate: Accelerate
+) {
+    fun race() {
+        if(accelerate.press() == Motion.MOVE){
+            car.move()
+        }
+    }
+}
