1990330917d22f0df411679c329ff6ae304b2005
taiyoung <qq3222@naver.com>
2025-01-21 23:46:35 +0900

    refactor: game 책임 분리

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/game/Racing.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/game/Racing.kt
@@ -1,4 +1,20 @@
 package org.gonza.kotlinplayground.game
 
-class Racing {
+import org.gonza.kotlinplayground.racer.Racer
+import org.gonza.kotlinplayground.racer.Racers
+import org.gonza.kotlinplayground.rule.Judge
+
+class Racing(
+    private val racers: Racers,
+    private val judge: Judge
+) {
+    fun play(turns: Int) {
+        racers.race(turns)
+        val winners = judge.winner(racers.getRacers())
+        announceWinners(winners)
+    }
+
+    private fun announceWinners(winners: List<Racer>) {
+        println("\n${winners.joinToString(", ") { it.car.name }}가 최종 우승했습니다.")
+    }
 }
--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/game/RacingTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/game/RacingTest.kt
@@ -1,4 +1,28 @@
 package org.gonza.kotlinplayground.game
 
+import org.gonza.kotlinplayground.Fixture.racer1
+import org.gonza.kotlinplayground.Fixture.racer2
+import org.gonza.kotlinplayground.racer.Racers
+import org.gonza.kotlinplayground.rule.Judge
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
 class RacingTest {
+    val judge = Judge()
+    @Test
+    fun `Racers로 게임을 진행할 수 있다`(){
+        val turns = 5
+        val racers= Racers(
+            listOf(racer1, racer2)
+        )
+
+        racers.race(turns)
+
+        assertDoesNotThrow {
+            Racing(
+                racers,
+                judge
+            )
+        }
+    }
 }
