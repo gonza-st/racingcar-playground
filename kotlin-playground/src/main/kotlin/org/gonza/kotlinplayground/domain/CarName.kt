ec9e48e99aaed1ad19147a23ab6c33f35bc2a6e1
Choi_Kyle <az86067@gmail.com>
2025-01-21 21:10:07 +0900

    refactor: CarName class

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/CarName.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/CarName.kt
@@ -1,15 +1,13 @@
 package org.gonza.kotlinplayground.domain
 
-class CarName {
-    constructor(value: String) {
-        if(isOverFive(value)) {
-            throw Exception("name must be lte 5")
-        }
-
-        this.value = value
-    }
-
+class CarName(
     val value: String
+) {
+    init {
+        if(isOverFive(value)) {
+            throw IllegalArgumentException("name must be lte 5")
+        }
+    }
 
     private fun isOverFive(value: String): Boolean = value.length > 5
 }
