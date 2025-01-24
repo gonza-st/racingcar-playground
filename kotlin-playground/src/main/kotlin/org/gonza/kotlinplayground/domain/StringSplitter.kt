d9e9ee0b519c1e9f8f0a7b85049bd6641fa718cd
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2025-01-24 13:37:27 +0900

    refactor: 네이밍 수정으로 책임감 도메인에서 분리
    네이밍 수정으로 책임감 도메인에서 분리

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/StringSplitter.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/StringSplitter.kt
@@ -1,13 +1,13 @@
 package org.gonza.kotlinplayground.domain
 
-class NameSplitter {
+class StringSplitter {
     companion object {
         private const val SPLIT_DELIMITER = ","
         private const val BLANK_STRING = ""
 
         fun split(value: String): List<String> {
             if (value.isEmpty()) {
-                throw IllegalArgumentException("이름은 빈 문자열일 수 없습니다")
+                throw IllegalArgumentException("나누려는 문자열은 빈 문자열일 수 없습니다")
             }
             val nameList = value.split(SPLIT_DELIMITER).map { it.trim() }
             validate(nameList)
@@ -19,11 +19,11 @@ class NameSplitter {
             val nameSet = nameList.toSet()
 
             if (nameSet.contains(BLANK_STRING)) {
-                throw IllegalArgumentException("이름은 빈 문자열일 수 없습니다")
+                throw IllegalArgumentException("나누려는 문자열은 빈 문자열일 수 없습니다")
             }
 
             if (nameSet.size != nameList.size) {
-                throw IllegalArgumentException("이름은 중복될 수 없습니다")
+                throw IllegalArgumentException("나누려는 문자열은 중복일 수 없습니다")
             }
         }
     }
