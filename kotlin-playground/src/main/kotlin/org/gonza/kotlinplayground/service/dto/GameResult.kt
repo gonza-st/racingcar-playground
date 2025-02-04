3356057ae35ff819df4bf784ddb8f7a60a23c486
leeMK09 <dlaudrb09@naver.com>
2025-02-04 13:57:03 +0900

    chore: gameCount를 tryCount 로 변경

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/dto/GameResult.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/dto/GameResult.kt
@@ -1,16 +1,16 @@
 package org.gonza.kotlinplayground.service.dto
 
-import org.gonza.kotlinplayground.service.Round
 import org.gonza.kotlinplayground.domain.car.Car
+import org.gonza.kotlinplayground.service.Round
 import org.gonza.kotlinplayground.service.vo.TryCount
 
 data class GameResult(
     val tryCount: TryCount,
-    val movedCarList: List<Car>
+    val movedCarList: List<Car>,
 )
 
 fun GameResult.toRacingCarGame(): Round =
     Round(
         carList = this.movedCarList,
-        currentGameCount = this.tryCount
+        currentTryCount = this.tryCount,
     )
